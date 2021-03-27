560734f47d1157dba0de942ac51bc4fecd2ae77f
Michal Ormos <michal.ormos@sewio.net>
2021-03-27 16:33:46 +0100

    [+] parser

---
+++ src/finder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# name finder.py
+# author Michal Ormos (mi.ormos@gmail.com)
+# date 27.3.2021
+# license MIT
+
+# import parser
+from parser import array
+
+# parser.init()
+print(array)
